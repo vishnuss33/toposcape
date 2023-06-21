6874d03e2d184f51cd0300c7495a761978642cd0
vishnu s s <vishnutvm33@gmail.com>
2023-06-21 13:37:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+$(document).ready(function () {
 
+  function resize() {
+    if ($(window).width() < $(window).height()) {
+      $('.offcanvas-end').removeClass('offcanvas').addClass('container-fluid');
+      $('.col-lg-12').css("position", "fixed");
+      $('.right-btn').css("display", "none");
+      $('.btn-close').css("display", "none");
+      $('viewer').css("height", "910");
+
+    }
+    console.log("window.width", $(window).width());
+  }
+
+  //watch window resize
+  $(window).on('resize', function () {
+    resize()
+  });
+
+  resize()
+
+
+});
